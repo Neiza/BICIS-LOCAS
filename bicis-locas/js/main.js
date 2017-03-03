afb367c1ae5905ab2a328a61f518471a96bc8524
Neiza Nunez <neizapf@gmail.com>
2017-03-03 15:20:04 -0500

    Segundo commit

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -5,14 +5,33 @@ function validateForm(){
         buttonValidation.addEventListener('click', function(event){
               event.preventDefault();
               var inputName     = document.getElementById('name');
-             var inputLastName = document.getElementById('lastname');
+              var inputLastName = document.getElementById('lastname');
               var inputEmail    = document.getElementById('input-email');
               var inputPassword = document.getElementById('input-password');
+              var bicycles      = document.getElementsByClassName("form-control")[4];
+
+              var expressionOnlyLetters          = /^[A-Za-z]+$/;
+              var expressionFirstLetterUppercase = /^[A-Z]/;
+              var expressionEmail                = /\w+@+\w+\.+[a-z]/;
+              var expressionPassword6Characters  =/^[A-Za-z0-9_]{1,6}$/;
 
              return (inputName.value    =="") ? alert("Enter your name"):
                     (inputLastName.value=="" )? alert("Enter your last name"):
-                    (inputEmail.value  =="") ? alert("Enter your e-mail"):
-                    (inputPassword.value=="") ? alert("Enter your password"):"";
+                    (inputEmail.value   =="") ? alert("Enter your e-mail"):
+                    (inputPassword.value=="") ? alert("Enter your password"):
+
+                    (!expressionOnlyLetters.test(inputName.value))     ? alert("Name can contain only letters"):
+                    (!expressionOnlyLetters.test(inputLastName.value)) ? alert("Last Name can contain only letters"):
+
+                    (!expressionFirstLetterUppercase.test(inputName.value))     ? alert("Name should starts with a capital"):
+                    (!expressionFirstLetterUppercase.test(inputLastName.value)) ? alert("Last Name should starts with a capital"):
+
+                    (!expressionEmail.test(inputEmail.value)) ? alert("Enter a valid e-mail"):
+
+                    (!expressionPassword6Characters.test(inputPassword.value)) ? alert("Password should have only 6 characters"):
+                    (inputPassword.value=="password" || inputPassword.value=="123456" ||inputPassword.value=="098754") ? alert("That's not a valid password"):
+                    (bicycles.value=="0") ? alert("Choose a bicycle")  :
+                    "";
 
         })
   })
